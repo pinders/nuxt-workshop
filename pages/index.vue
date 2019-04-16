fa36774ab399858546d95883e8c6969d3214277c
Erik Pinders <erik.pinders@gmail.com>
2019-04-16 07:55:40 +0200

    Add some instructions

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,36 @@
+<!--
+  Default Markup for Vuetify applications:
+  https://vuetifyjs.com/en/framework/default-markup
+
+  <v-app>
+    // We don't need the navigation drawer just yet
+    <v-navigation-drawer app />
+
+    <v-toolbar app />
+
+    <v-content>
+      <v-container fluid>
+        // This can be replaced with our actual page
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+
+    // We don't need a footer in our app
+    <v-footer app></v-footer>
+  </v-app>
+
+  Documentation
+  - v-toolbar: https://vuetifyjs.com/en/components/toolbars
+
+  Other useful Components
+  - grid: https://vuetifyjs.com/en/framework/grid
+  - grid-list: https://vuetifyjs.com/en/framework/grid-lists
+  - v-card: https://vuetifyjs.com/en/components/cards
+
+  TASK: Update the template section and make the app look like the one from 2-layouts
+
+-->
+
 <template>
   <div />
 </template>
